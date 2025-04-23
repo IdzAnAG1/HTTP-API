ecd41c0129088d50b03bdc1ddb6de60a786f019a
SusanoNoMikoto <Susan0N0Mik0t0@outlook.com>
2025-04-23 10:24:10 +0300

    Third commit. Add GET Methods for getting info about all tasks and info about a task by its ID

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,23 @@
 package main
+
+import (
+	"FastAPI/internal/handlers"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+)
+
+func main() {
+	r := mux.NewRouter()
+
+	r.HandleFunc("/task", handlers.NewTaskHandler).Methods("POST")
+	r.HandleFunc("/tasks", handlers.ShowAllTasksHandler).Methods("GET")
+	r.HandleFunc("/task/{id}", handlers.ShowTaskHandler).Methods("GET")
+
+	port := ":8080"
+	log.Printf("Server is running on %s", port)
+	err := http.ListenAndServe(port, r)
+	if err != nil {
+		log.Fatalf("Error starting server: %v", err)
+	}
+}
